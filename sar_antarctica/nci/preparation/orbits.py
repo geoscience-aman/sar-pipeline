8016828b4fc3129ebcf67b1f9a6fb8cd33f84ea6
Caitlin Adams <25995927+caitlinadams@users.noreply.github.com>
2025-01-08 13:13:52 +1100

    Add test for find_latest_orbit_for_scene

--- sar_antarctica/nci/preparation/orbits.py
+++ sar_antarctica/nci/preparation/orbits.py
@@ -1,29 +1,36 @@
 from datetime import datetime
 from pathlib import Path
 import re
+from typing import Optional
 
-from sar_antarctica.nci.preparation.scenes import parse_scene_file_dates
+from sar_antarctica.nci.preparation.scenes import parse_scene_file_dates, parse_scene_file_sensor
 
 # Constants for NCI
 S1_DIR = Path("/g/data/fj7/Copernicus/Sentinel-1/")
 POE_DIR = "POEORB"
 RES_DIR = "RESORB"
-ORBIT_DIRS = [POE_DIR, RES_DIR]
-SENSORS = ["S1A", "S1B"]
 
 def parse_orbit_file_dates(orbit_file_name: str) -> tuple[datetime, datetime, datetime]:
-    """
-    Extracts published_date, start_date, and end_date from the given orbit file.
+    """Extracts published_date, start_date, and end_date from the given orbit file.
     Filename example: S1A_OPER_AUX_POEORB_OPOD_20141207T123431_V20141115T225944_20141117T005944.EOF
     - Published: 20141207T123431
     - Start: 20141115T225944
     - End: 20141117T005944
 
-    Args:
-        file_name (str): The orbit file name as a string.
+    Parameters
+    ----------
+    orbit_file_name : str
+        The orbit file name as a string.
 
-    Returns:
-        tuple(datetime): a tuple of datetimes for published, start and end of the orbit file
+    Returns
+    -------
+    tuple[datetime, datetime, datetime]
+        a tuple of datetimes for published, start and end of the orbit file
+
+    Raises
+    ------
+    ValueError
+        Did not find a match to the expected date pattern of published_date followed by start_date and end_date
     """
     # Regex pattern to match the dates
     pattern = (r"(?P<published_date>\d{8}T\d{6})_V"
@@ -43,34 +50,60 @@ def parse_orbit_file_dates(orbit_file_name: str) -> tuple[datetime, datetime, da
 
     return (published_date, start_date, stop_date)
 
-def find_latest_orbit_for_scene(scene_id: str, poe_only: bool = True) -> Path:
-    """
-    Identifies the most recent orbit file available for a given scene, based 
+def find_latest_orbit_for_scene(scene_id: str, orbit_type: Optional[str] = None) -> Path:
+    """Identifies the most recent orbit file available for a given scene, based 
     on the scene's start and end date.
+
+    Parameters
+    ----------
+    scene_id : str
+        Sentinel-1 scene ID
+        e.g. S1A_EW_GRDM_1SDH_20220612T120348_20220612T120452_043629_053582_0F6
+    orbit_type : Optional[str], optional
+        Any of "POE" for POE orbits, "RES" for RES orbits, or None, by default None
+
+    Returns
+    -------
+    Path
+        Full file path to latest orbit file on NCI
+
+    Raises
+    ------
+    ValueError
+        orbit_type must be one of "POE", "RES" or None
+    ValueError
+        No valid orbit file was found
     """
 
     scene_start, scene_stop = parse_scene_file_dates(scene_id)
+    scene_sensor = parse_scene_file_sensor(scene_id)
 
     relevant_orbits = []
 
-    for orbit_dir in ORBIT_DIRS:
+    if orbit_type == "POE":
+        orbit_directories = [POE_DIR]
+    elif orbit_type == "RES":
+        orbit_directories = [RES_DIR]
+    elif orbit_type is None:
+        orbit_directories = [RES_DIR, POE_DIR]
+    else:
+        raise ValueError("orbit_type must be one of 'POE', 'RES', or None")
+
+    # Find all orbits for the sensor that fall within the date range of the scene
+    for orbit_dir in orbit_directories:
         orbit_dir_path = S1_DIR / orbit_dir
-        for sensor in SENSORS:
-            orbit_files_path = orbit_dir_path / sensor
-            orbit_files = orbit_files_path.glob("*.EOF")
+        orbit_files_path = orbit_dir_path / scene_sensor
+        orbit_files = orbit_files_path.glob("*.EOF")
 
-            for orbit_file in orbit_files:
+        for orbit_file in orbit_files:
 
-                orbit_published, orbit_start, orbit_stop = parse_orbit_file_dates(orbit_file)
-                
-                # Check if scene falls within orbit 
-                if scene_start >= orbit_start and scene_stop <= orbit_stop:
-                    orbit_metadata = (orbit_file, orbit_dir, orbit_published)
-                    relevant_orbits.append(orbit_metadata)
+            orbit_published, orbit_start, orbit_stop = parse_orbit_file_dates(orbit_file)
+            
+            # Check if scene falls within orbit 
+            if scene_start >= orbit_start and scene_stop <= orbit_stop:
+                orbit_metadata = (orbit_file, orbit_dir, orbit_published)
+                relevant_orbits.append(orbit_metadata)
 
-    if poe_only:
-        relevant_orbits = [item for item in relevant_orbits if item[1] == POE_DIR]
-    
     # If relevant_orbits is empty, set latest_orbit to None
     latest_orbit = max(relevant_orbits, key=lambda x: x[2]) if relevant_orbits else None
 
--- tests/test_orbits.py
+++ tests/test_orbits.py
@@ -2,7 +2,7 @@ from sar_antarctica.nci.preparation.orbits import (
     parse_orbit_file_dates,
     find_latest_orbit_for_scene
 )
-
+from pathlib import Path
 import pytest
 import dataclasses
 from datetime import datetime
@@ -33,3 +33,33 @@ orbits = [orbit_1, orbit_2]
 def test_parse_orbit_file_dates(orbit: Orbit):
     date_tuple = (orbit.published_date, orbit.start_date, orbit.stop_date)
     assert parse_orbit_file_dates(orbit.file) == date_tuple
+
+
+@dataclasses.dataclass
+class Scene:
+    scene_id: str
+    latest_orbit: Path
+    latest_res_orbit: Path
+    latest_poe_orbit: Path
+
+scene_1 = Scene(
+    scene_id="S1B_EW_GRDM_1SDH_20191130T165626_20191130T165726_019159_0242A2_2F58",
+    latest_orbit=Path("/g/data/fj7/Copernicus/Sentinel-1/POEORB/S1B/S1B_OPER_AUX_POEORB_OPOD_20191220T110516_V20191129T225942_20191201T005942.EOF"),
+    latest_res_orbit=Path("/g/data/fj7/Copernicus/Sentinel-1/RESORB/S1B/S1B_OPER_AUX_RESORB_OPOD_20191130T210136_V20191130T154804_20191130T190534.EOF"),
+    latest_poe_orbit=Path("/g/data/fj7/Copernicus/Sentinel-1/POEORB/S1B/S1B_OPER_AUX_POEORB_OPOD_20191220T110516_V20191129T225942_20191201T005942.EOF")
+)
+
+scene_2 = Scene(
+    scene_id="S1A_EW_GRDM_1SDH_20220612T120348_20220612T120452_043629_053582_0F66",
+    latest_orbit=Path("/g/data/fj7/Copernicus/Sentinel-1/POEORB/S1A/S1A_OPER_AUX_POEORB_OPOD_20220702T081845_V20220611T225942_20220613T005942.EOF"),
+    latest_res_orbit=Path("/g/data/fj7/Copernicus/Sentinel-1/RESORB/S1A/S1A_OPER_AUX_RESORB_OPOD_20220612T143829_V20220612T104432_20220612T140202.EOF"),
+    latest_poe_orbit=Path("/g/data/fj7/Copernicus/Sentinel-1/POEORB/S1A/S1A_OPER_AUX_POEORB_OPOD_20220702T081845_V20220611T225942_20220613T005942.EOF")
+)
+
+scenes = [scene_1, scene_2]
+
+@pytest.mark.parametrize("scene", scenes)
+def test_find_latest_orbit_for_scene(scene: Scene):
+    assert find_latest_orbit_for_scene(scene.scene_id) == scene.latest_orbit
+    assert find_latest_orbit_for_scene(scene.scene_id, orbit_type="RES") == scene.latest_res_orbit
+    assert find_latest_orbit_for_scene(scene.scene_id, orbit_type="POE") == scene.latest_poe_orbit
