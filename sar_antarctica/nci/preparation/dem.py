6f746020529e0f6d5fbe08325df012a3d3094042
abradley60 <alexbradley60@gmail.com>
2025-01-10 17:20:37 +1100

    bugfix for filling dem, clean and generalise for AM

--- sar_antarctica/nci/preparation/dem.py
+++ sar_antarctica/nci/preparation/dem.py
@@ -6,32 +6,41 @@ import logging
 from affine import Affine
 import rasterio
 from rasterio.crs import CRS
+from rasterio.transform import array_bounds
 
+from .geoid import remove_geoid
 from ...utils.raster import (
     expand_raster_to_bounds, 
     reproject_raster,
-    merge_raster_files
+    merge_raster_files,
+    bounds_from_profile
 )
-from .geoid import remove_geoid
+from ...utils.spatial import (
+    adjust_bounds, 
+    get_local_utm,
+)
 
+# paths
 COP30_FOLDER_PATH = '/g/data/v10/eoancillarydata-2/elevation/copernicus_30m_world/'
 GEOID_PATH = '/g/data/yp75/projects/ancillary/geoid/us_nga_egm2008_1_4326__agisoft.tif'
 
 def get_cop30_dem_for_bounds(bounds: tuple, save_path: str, ellipsoid_heights: bool = True): 
     
-    logging.info(f'Getting cop30m dem for bounds: {bounds}')
+    logging.info(f'Getting cop30m dem that covers bounds: {bounds}')
     # check if scene crosses the AM
     antimeridian_crossing = check_s1_bounds_cross_antimeridian(bounds, max_scene_width=8)
     if antimeridian_crossing:
-        logging.warning('Scene crosses the dateline/antimeridian')
-        # TODO assumes only antarctic data (3031). Elsewhere the local UTM zone should be used
-        logging.warning('Data will be returned in 3031 projection') 
+        logging.warning('DEM crosses the dateline/antimeridian')
+        logging.info('Finding best crs for area')
+        target_crs = get_target_antimeridian_projection(bounds)
+        logging.warning(f'Data will be returned in EPSG:{target_crs} projection') 
         # split the scene into left and right
         logging.info(f'Splitting bounds into left and right side of antimeridian')
         bounds_left, bounds_right = split_s1_bounds_at_am_crossing(bounds, lat_buff=0)
         logging.info(f'Bounds left: {bounds_left}')
-        logging.info(f'Bounds left: {bounds_right}')
-        # use recursion like a legend to create dems for the left and right side
+        logging.info(f'Bounds right: {bounds_right}')
+        # use recursion like a legend to create dems for the left and right side using
+        # when passed back into the top function, these will be created and then merged
         left_save_path = '.'.join(save_path.split('.')[0:-1]) + "_left." + save_path.split('.')[-1]
         logging.info(f'Getting tiles for left bounds')
         get_cop30_dem_for_bounds(bounds_left, left_save_path, ellipsoid_heights)
@@ -39,19 +48,18 @@ def get_cop30_dem_for_bounds(bounds: tuple, save_path: str, ellipsoid_heights: b
         logging.info(f'Getting tiles for right bounds')
         get_cop30_dem_for_bounds(bounds_right, right_save_path, ellipsoid_heights)
         # reproject to 3031 and merge
-        logging.info(f'Reprojecting left and right side of antimeridian to polar stereographic EPGS:3031')
-        reproject_raster(left_save_path, left_save_path, 3031)
-        reproject_raster(right_save_path, right_save_path, 3031)
+        logging.info(f'Reprojecting left and right side of antimeridian to EPGS:{target_crs}')
+        reproject_raster(left_save_path, left_save_path, target_crs)
+        reproject_raster(right_save_path, right_save_path, target_crs)
         logging.info(f'Merging across antimeridian')
-        merge_raster_files([left_save_path, right_save_path], output_path=save_path, nodata_value=np.nan)
-        os.remove(left_save_path)
-        os.remove(right_save_path)
-        with rasterio.open(save_path) as dem:
-                dem_profile = dem.profile
-                dem_arr = dem.read()
+        dem_arr, dem_profile = merge_raster_files([left_save_path, right_save_path], output_path=save_path, nodata_value=np.nan)
+        #os.remove(left_save_path)
+        #os.remove(right_save_path)
         return dem_arr, dem_profile
     else:
-        # get a list of the required dem paths
+        # logging.info(f'Expanding bounds') # TODO this should sit outside of this function
+        # bounds = expand_bounds(bounds, buffer=0.1)
+        logging.info(f'Getting cop30m dem for bounds: {bounds}')
         logging.info(f'Searching folder for dem tiles covering scene: {COP30_FOLDER_PATH}')
         dem_paths = find_required_dem_tile_paths(bounds)
         logging.info(f'Dem tiles found: {len(dem_paths)}')
@@ -60,27 +68,27 @@ def get_cop30_dem_for_bounds(bounds: tuple, save_path: str, ellipsoid_heights: b
             logging.info('Creating an empty profile for cop30m DEM')
             dem_profile = make_empty_cop30m_profile(bounds)
             logging.info('Filling dem with zero values based on profile')
-            expand_raster_to_bounds(bounds, src_profile=dem_profile, save_path=save_path, fill_value=0)
+            dem_arr, dem_profile = expand_raster_to_bounds(bounds, src_profile=dem_profile, save_path=save_path, fill_value=0)
         else:
             logging.info(f'Merging dem tiles and saving to: {save_path}')
-            merge_raster_files(dem_paths, save_path, nodata_value=np.nan)
+            dem_arr, dem_profile = merge_raster_files(dem_paths, save_path, nodata_value=np.nan)
         logging.info(f'Check the dem covers the required bounds')
-        with rasterio.open(save_path) as dem:
-            dem_bounds = tuple(dem.bounds)
+        dem_bounds = bounds_from_profile(dem_profile)
         logging.info(f'Dem bounds: {dem_bounds}')
         logging.info(f'Target bounds: {bounds}')
-        bounds_filled_by_dem = box(*dem_bounds).contains(box(*bounds)) # TODO write test
+        bounds_filled_by_dem = box(*bounds).within(box(*dem_bounds))
         logging.info(f'Dem covers target: {bounds_filled_by_dem}')
         if not bounds_filled_by_dem:
             fill_value = 0
             logging.info(f'Expanding bounds with fill value: {fill_value}')
-            expand_raster_to_bounds(bounds, src_path=save_path, save_path=save_path, fill_value=0)
-            with rasterio.open(save_path) as dem:
-                dem_bounds = tuple(dem.bounds)
+            dem_arr, dem_profile = expand_raster_to_bounds(
+                bounds, 
+                src_profile=dem_profile, 
+                src_array=dem_arr, 
+                save_path=save_path, 
+                fill_value=0)
+            dem_bounds = bounds_from_profile(dem_profile)
             logging.info(f'Expanded dem bounds: {dem_bounds}')
-        with rasterio.open(save_path) as dem:
-                dem_profile = dem.profile
-                dem_arr = dem.read()
         if ellipsoid_heights:
             logging.info(f'Subtracting the geoid from the DEM to return ellipsoid heights')
             logging.info(f'Using geoid file: {GEOID_PATH}')
@@ -93,13 +101,39 @@ def get_cop30_dem_for_bounds(bounds: tuple, save_path: str, ellipsoid_heights: b
                 save_path=save_path,
             )
         return dem_arr, dem_profile
-            
+
+def expand_bounds(bounds: tuple, buffer: float):
+    """
+    """
+    min_lat = min(bounds[1], bounds[3])
+    if min_lat < -50:
+        # adjust the bounds at high southern latitudes
+        bounds = adjust_bounds(bounds, src_crs=4326, ref_crs=3031)
+    if min_lat > -50:
+        # adjust the bounds at high norther latitudes
+        bounds = adjust_bounds(bounds, src_crs=4326, ref_crs=3995)
+    exp_bounds = list(box(*bounds).buffer(buffer).bounds)
+    exp_bounds[0] = bounds[0] if exp_bounds[0] < -180 else exp_bounds[0] # keep original
+    exp_bounds[2] = bounds[2] if exp_bounds[2] > 180 else exp_bounds[2] # keep original
+    return tuple(exp_bounds)
+
 def find_required_dem_tile_paths(bounds: tuple, check_exists : bool = True)->list[str]:
     """ generate a list of the required dem paths based on the bounding coords
     """
-    lat_range = list(range(int(np.floor(bounds[1])), int(np.ceil(bounds[3]+1))))
-    lon_range = list(range(int(np.floor(bounds[0])), int(np.ceil(bounds[2]+1))))
+    # handle negatives, i.e. if value is -77.6 we want to start at ceiling -77
+    # if value is 77.6 we want to start at floor -77
+    min_lat = np.floor(bounds[1]) if bounds[1] < 0 else np.ceil(bounds[1])
+    max_lat = np.ceil(bounds[3]) if bounds[3] < 0 else np.floor(bounds[3])+1
+    min_lon = np.floor(bounds[0]) if bounds[0] < 0 else np.floor(bounds[0])
+    max_lon = np.ceil(bounds[2]) if bounds[2] < 0 else np.ceil(bounds[2])
+    logging.info(f'lat min: {min_lat}, lat max: {max_lat}')
+    logging.info(f'lon min: {min_lon}, lon max: {max_lon}')
+    lat_range = list(range(int(min_lat), int(max_lat)))
+    lon_range = list(range(int(min_lon), int(max_lon)))
+    logging.info(f'lat tile range: {lat_range}')
+    logging.info(f'lon tile range: {lon_range}')
     dem_paths = []
+    dem_folders = []
 
     for lat in lat_range:
         for lon in lon_range:
@@ -112,8 +146,11 @@ def find_required_dem_tile_paths(bounds: tuple, check_exists : bool = True)->lis
                 # check the file exists, e.g. over water will not be a file
                 if os.path.exists(dem_path):
                     dem_paths.append(dem_path)
+                    dem_folders.append(dem_foldername)
             else:
                 dem_paths.append(dem_path)
+    for p in set(dem_folders):
+        logging.info(p)
     return list(set(dem_paths))
 
 def check_s1_bounds_cross_antimeridian(bounds : tuple, max_scene_width : int =20) -> bool:
@@ -162,6 +199,15 @@ def split_s1_bounds_at_am_crossing(bounds: tuple, lat_buff : float = 0) -> list[
     bounds_left = (-180, min_y, max_negative_x, max_y)
     bounds_right = (min_positive_x, min_y, 180, max_y)
     return [tuple(bounds_left), tuple(bounds_right)]
+
+def get_target_antimeridian_projection(bounds):
+    """depending where were are on the earth, the desired
+    crs at the antimeridian will change. e.g. polar stereo
+    is desired at high and low lats, local utm zone elsewhere
+    (e.g. at the equator)"""
+    min_lat = min(bounds[1], bounds[3])
+    target_crs =  3031 if min_lat < -50 else 3995 if min_lat > 50 else get_local_utm(bounds, antimeridian=True)
+    return target_crs
 
 def make_empty_cop30m_profile(bounds):
 
