8481bd01ce2c86d2eb228625b7eb7903f389f946
abradley60 <alexbradley60@gmail.com>
2025-01-10 17:18:57 +1100

    new test for AM crs

--- tests/sar_antarctica/test_dem.py
+++ tests/sar_antarctica/test_dem.py
@@ -1,6 +1,7 @@
 from sar_antarctica.nci.preparation.dem import (
     check_s1_bounds_cross_antimeridian,
-    split_s1_bounds_at_am_crossing
+    split_s1_bounds_at_am_crossing,
+    get_target_antimeridian_projection,
 ) 
 import shapely
 import pytest
@@ -27,6 +28,15 @@ def test_split_bounds_at_am_crossing(bounds, lat_buff, expected_left, expected_r
     assert all(a == pytest.approx(b,rel=1e-9) for a, b in zip(left, expected_left))
     assert all(a == pytest.approx(b,rel=1e-9) for a, b in zip(right, expected_right)) 
 
+@pytest.mark.parametrize("bounds, target_crs", [
+    ((-177.884048, -78.176201, 178.838364, -75.697151), 3031), # antarctic
+    ((-177.884048, 78.176201, 178.838364, 75.697151), 3995), # arctic
+    ((-177.884048, -1, 178.838364, 1), 32601), # centre slight left of equator
+    ((-178.884048, -1, 177.838364, 1), 32660), # centre slight right of equator
+])
+def test_get_target_antimeridian_projection(bounds, target_crs):
+    assert get_target_antimeridian_projection(bounds) == target_crs
+
 
 if __name__ == "__main__":
 
@@ -40,10 +50,12 @@ if __name__ == "__main__":
     logging.getLogger().setLevel(logging.INFO)
     
     # bounds = (163.121597, -78.632782, 172.382263, -76.383263) # full s1 scene
-    # bounds = (165, -76.632782, 170, -75) # smaller area
+    #bounds = (165, -76.632782, 170, -75) # smaller area
     # bounds = (-177.884048, -78.176201, 178.838364, -75.697151) # full AM scene bounds
-    # bounds = (-179, -78, 179, -77) # smaller AM bounds
-    bounds = (140, -66, 141, -65) # smaller area over water
+    bounds = (-177.2, -79.2, 178.1, -77.1) # smaller AM bounds
+    # bounds = (140, -66, 141, -65) # smaller area over water
+    # bounds = (20.1, -75.2, 22.2, -73.1) 
+    # bounds = (-22.2, -75.2, -20.1, -73.1) 
 
 
     #dem_paths = find_required_dem_tile_paths(bounds)
