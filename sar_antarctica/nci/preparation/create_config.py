8adeb5b3eefab9a3b596546071b243b01418e696
Caitlin Adams <25995927+caitlinadams@users.noreply.github.com>
2025-01-13 16:54:35 +1100

    Update create_config to remove workflow config and write scene config

--- sar_antarctica/nci/preparation/create_config.py
+++ sar_antarctica/nci/preparation/create_config.py
@@ -4,57 +4,58 @@ from pathlib import Path
 from sar_antarctica.nci.preparation.scenes import find_scene_file_from_id
 from sar_antarctica.nci.preparation.orbits import find_latest_orbit_for_scene
 
-
-
-
-def write_pyrosar_settings(config_file: Path):
-    with open(config_file, "a") as cf:
-        header = "[geocode]\n"
-        spacing_setting = "spacing = 40\n"
-        scaling_setting = "scaling = 'linear'\n"
-
-        cf.writelines([header, spacing_setting, scaling_setting, "\n"])
-
-
-def write_gamma_settings(config_file: Path):
-    with open(config_file, "a") as cf:
-        header = "[gamma]\n"
-        software_setting = "software_env_var = '/g/data/dg9/GAMMA/GAMMA_SOFTWARE-20230712'\n"
-        libs_setting = "libs_env_var = '/g/data/yp75/projects/pyrosar_processing/sar-pyrosar-nci:/apps/fftw3/3.3.10/lib:/apps/gdal/3.6.4/lib64'\n"
-
-        cf.writelines([header, software_setting, libs_setting, "\n"])
-
-def write_file_paths(config_file: Path, scene_path, orbit_path):
-    header = "[files]\n"
+def write_file_paths(config_file: Path, scene_path, orbit_path, dem_path, data_path, ancillary_dir="ancillary", processed_dir="processed_scene"):
+    inputs_header = "[inputs]\n"
     scene_setting = f"scene = '{str(scene_path)}'\n"
     orbit_setting = f"orbit = '{str(orbit_path)}'\n"
-    with open(config_file, "a") as cf:
-        cf.writelines([header, scene_setting, orbit_setting, "\n"])
+    dem_setting = f"dem = '{str(dem_path)}'\n"
+
+    outputs_header = "[outputs]\n"
+    data_path_setting = f"data = '{str(data_path)}'\n"
+    ancillary_setting = f"ancillary = '{ancillary_dir}'\n"
+    processed_setting = f"processed = '{processed_dir}'\n"
+
+    with open(config_file, "w") as cf:
+        cf.writelines([
+            inputs_header, 
+            scene_setting, 
+            orbit_setting,
+            dem_setting, 
+            "\n", 
+            outputs_header, 
+            data_path_setting,
+            ancillary_setting,
+            processed_setting
+        ])
         
 @click.command()
-@click.argument("scene_id")
-def main(scene_id: str):
+@click.argument("scene_id", nargs=1)
+@click.argument('scene_config', nargs=1)
+def main(scene_id: str, scene_config: str):
     print(f"Processing scene: {scene_id} \n")
 
-    config_path = Path("/g/data/yp75/ca6983/repositories/sar-antarctica/sar_antarctica/nci/processing/configs")
-    config_file = config_path / f"{scene_id}.toml"
-
-    write_gamma_settings(config_file)
-
+    # config_path = Path("/g/data/yp75/projects/sar-antractica-processing/config/scene_config")
+    config_file = Path(scene_config)
 
     # Identify location of scene on GADI
     scene_path = find_scene_file_from_id(scene_id)
-    print(scene_path)
 
     # Identify location of relevant orbit file on GADI
     latest_poe_file = find_latest_orbit_for_scene(scene_id, orbit_type="POE")
-    print(latest_poe_file)
 
     # Identify location of DEM/process DEM
+    dem_path = f"{scene_id}.tif"
 
-    write_file_paths(config_file, scene_path, latest_poe_file)
+    data_path = Path("/g/data/yp75/projects/sar-antractica-processing/data")
 
-    write_pyrosar_settings(config_file)
+    # Write to config file
+    write_file_paths(
+        config_file, 
+        scene_path, 
+        latest_poe_file, 
+        dem_path, 
+        data_path
+    )
 
 if __name__ == "__main__":
 
