c01f721eca5e54b9d45d852ae24167a46d98ed9e
abradley60 <alexbradley60@gmail.com>
2025-01-13 13:02:24 +1100

    docstrings and gpkg dem tile search

--- sar_antarctica/nci/preparation/dem.py
+++ sar_antarctica/nci/preparation/dem.py
@@ -2,11 +2,13 @@ import shapely
 from shapely.geometry import box
 import numpy as np
 import os
+from pathlib import Path
 import logging
 from affine import Affine
 import rasterio
 from rasterio.crs import CRS
 from rasterio.transform import array_bounds
+import geopandas as gpd
 
 from .geoid import remove_geoid
 from ...utils.raster import (
@@ -20,11 +22,33 @@ from ...utils.spatial import (
     get_local_utm,
 )
 
-# paths
-COP30_FOLDER_PATH = '/g/data/v10/eoancillarydata-2/elevation/copernicus_30m_world/'
-GEOID_PATH = '/g/data/yp75/projects/ancillary/geoid/us_nga_egm2008_1_4326__agisoft.tif'
+COP30_FOLDER_PATH = Path('/g/data/v10/eoancillarydata-2/elevation/copernicus_30m_world/')
+GEOID_PATH = Path('/g/data/yp75/projects/ancillary/geoid/us_nga_egm2008_1_4326__agisoft.tif')
+GPKG_PATH = Path('/g/data/yp75/projects/ancillary/dem/copdem_tindex.gpkg')
 
-def get_cop30_dem_for_bounds(bounds: tuple, save_path: str, ellipsoid_heights: bool = True): 
+def get_cop30_dem_for_bounds(
+        bounds: tuple, 
+        save_path: Path, 
+        ellipsoid_heights: bool = True
+        ) -> tuple[np.ndarray, dict]:
+    """Logic for acquiting the cop30m DEM for a given set of bounds on the NCI. The returned
+    dem will fully encompass the specified bounds. There may be additional data outside of
+    the bounds as all data from the merged tiles is returned.
+
+    Parameters
+    ----------
+    bounds : tuple
+        the set of bounds (min_lon, min_lat, max_lon, max_lat)
+    save_path : Path
+        Path where the DEM.tif should be saved
+    ellipsoid_heights : bool, optional
+        Return ellipsoid referenced heights by subtracting the geoid, by default True
+
+    Returns
+    -------
+    tuple [np.darray, dict]
+        dem array and dem rasterio profile
+    """
     
     logging.info(f'Getting cop30m dem that covers bounds: {bounds}')
     # check if scene crosses the AM
@@ -39,8 +63,9 @@ def get_cop30_dem_for_bounds(bounds: tuple, save_path: str, ellipsoid_heights: b
         bounds_left, bounds_right = split_s1_bounds_at_am_crossing(bounds, lat_buff=0)
         logging.info(f'Bounds left: {bounds_left}')
         logging.info(f'Bounds right: {bounds_right}')
-        # use recursion like a legend to create dems for the left and right side using
-        # when passed back into the top function, these will be created and then merged
+        # use recursion to create dems for the left and right side of AM
+        # when passed back into the top function, this section will be skipped creating
+        # A valid dem for each side which we can then merge at the desired CRS
         left_save_path = '.'.join(save_path.split('.')[0:-1]) + "_left." + save_path.split('.')[-1]
         logging.info(f'Getting tiles for left bounds')
         get_cop30_dem_for_bounds(bounds_left, left_save_path, ellipsoid_heights)
@@ -53,15 +78,16 @@ def get_cop30_dem_for_bounds(bounds: tuple, save_path: str, ellipsoid_heights: b
         reproject_raster(right_save_path, right_save_path, target_crs)
         logging.info(f'Merging across antimeridian')
         dem_arr, dem_profile = merge_raster_files([left_save_path, right_save_path], output_path=save_path, nodata_value=np.nan)
-        #os.remove(left_save_path)
-        #os.remove(right_save_path)
+        os.remove(left_save_path)
+        os.remove(right_save_path)
         return dem_arr, dem_profile
     else:
         # logging.info(f'Expanding bounds') # TODO this should sit outside of this function
         # bounds = expand_bounds(bounds, buffer=0.1)
         logging.info(f'Getting cop30m dem for bounds: {bounds}')
         logging.info(f'Searching folder for dem tiles covering scene: {COP30_FOLDER_PATH}')
-        dem_paths = find_required_dem_tile_paths(bounds)
+        #dem_paths = find_required_dem_tile_paths_by_filename(bounds)
+        dem_paths = find_required_dem_paths_from_geopackage(bounds)
         logging.info(f'Dem tiles found: {len(dem_paths)}')
         if len(dem_paths) == 0:
             logging.warning('No DEM files found, scene is over water or paths cannot be found')
@@ -97,13 +123,28 @@ def get_cop30_dem_for_bounds(bounds: tuple, save_path: str, ellipsoid_heights: b
                 dem_profile=dem_profile,
                 geoid_path = GEOID_PATH,
                 dem_area_or_point = 'Point',
-                res_buffer = 2,
+                buffer_pixels = 2,
                 save_path=save_path,
             )
         return dem_arr, dem_profile
 
-def expand_bounds(bounds: tuple, buffer: float):
-    """
+def expand_bounds(bounds: tuple, buffer: float) -> tuple:
+    """Expand the bounds for high lattitudes, and add a buffer. The
+    provided bounds sometimes do not contain the full scene due to
+    warping at high latitudes.
+
+    Parameters
+    ----------
+    bounds : tuple
+        the set of bounds (min_lon, min_lat, max_lon, max_lat)
+    buffer : float
+        The buffer to add to the bounds after they have been adjusted
+        if at high latitude
+
+    Returns
+    -------
+    tuple
+        the expanded bounds (min_lon, min_lat, max_lon, max_lat)
     """
     min_lat = min(bounds[1], bounds[3])
     if min_lat < -50:
@@ -117,17 +158,53 @@ def expand_bounds(bounds: tuple, buffer: float):
     exp_bounds[2] = bounds[2] if exp_bounds[2] > 180 else exp_bounds[2] # keep original
     return tuple(exp_bounds)
 
-def find_required_dem_tile_paths(bounds: tuple, check_exists : bool = True)->list[str]:
-    """ generate a list of the required dem paths based on the bounding coords
+def find_required_dem_paths_from_geopackage(
+        bounds: tuple, 
+        GPKG_PATH = GPKG_PATH,
+) -> list[str]: 
+    
+    gdf = gpd.read_file(GPKG_PATH)
+    bounding_box = box(*bounds)
+    
+    if gdf.crs is not None:
+        # ensure same crs
+        bounding_box = gpd.GeoSeries([bounding_box], crs="EPSG:4326").to_crs(gdf.crs).iloc[0]
+
+    # Find rows that intersect with the bounding box
+    intersecting_tiles = gdf[gdf.intersects(bounding_box)]
+    if len(intersecting_tiles) > 0:
+        return intersecting_tiles.location.tolist()
+    else:
+        return []
+
+
+def find_required_dem_tile_paths_by_filename(
+        bounds: tuple, 
+        check_exists : bool = True, 
+        COP30_FOLDER_PATH: Path = COP30_FOLDER_PATH
+        )->list[str]:
+    """generate a list of the required dem paths based on the bounding coords. The 
+    function searches the specified folder.
+
+    Parameters
+    ----------
+    bounds : tuple
+        the set of bounds (min_lon, min_lat, max_lon, max_lat)
+    check_exists : bool, optional
+        Check if the file exists, by default True
+    COP30_FOLDER_PATH : str, optional
+        path to the tile folders, by default COP30_FOLDER_PATH
+
+    Returns
+    -------
+    list[str]
+        list of paths for required dem tiles in bounds
     """
-    # handle negatives, i.e. if value is -77.6 we want to start at ceiling -77
-    # if value is 77.6 we want to start at floor -77
+    # logic to find the correct files based on data being stored in each tile folder
     min_lat = np.floor(bounds[1]) if bounds[1] < 0 else np.ceil(bounds[1])
     max_lat = np.ceil(bounds[3]) if bounds[3] < 0 else np.floor(bounds[3])+1
     min_lon = np.floor(bounds[0]) if bounds[0] < 0 else np.floor(bounds[0])
     max_lon = np.ceil(bounds[2]) if bounds[2] < 0 else np.ceil(bounds[2])
-    logging.info(f'lat min: {min_lat}, lat max: {max_lat}')
-    logging.info(f'lon min: {min_lon}, lon max: {max_lon}')
     lat_range = list(range(int(min_lat), int(max_lat)))
     lon_range = list(range(int(min_lon), int(max_lon)))
     logging.info(f'lat tile range: {lat_range}')
@@ -154,20 +231,22 @@ def find_required_dem_tile_paths(bounds: tuple, check_exists : bool = True)->lis
     return list(set(dem_paths))
 
 def check_s1_bounds_cross_antimeridian(bounds : tuple, max_scene_width : int =20) -> bool:
-    """Check if the s1 scene bounds cross the antimeridian. The bounds are valid, just very
-    large so we set a max scene width of 10. if the scene is greater than this either side
-    of the dateline, we assume it crosses the dateline. Alternate scenario is a bounds 
-    with a very large width (close to earths width).
+    """Check if the s1 scene bounds cross the antimeridian. The bounds of a sentinel-1 
+    are valid at the antimeridian, just very large. By setting a max scene width, we
+    can determine if the antimeridian is crossed. Alternate scenario is a bounds 
+    with a very large width (i.e. close to the width of the earth).
 
-    e.g. [-178.031982, -71.618958, 178.577438, -68.765755]
+    Parameters
+    ----------
+    bounds : tuple
+        the set of bounds (min_lon, min_lat, max_lon, max_lat)
+    max_scene_width : int, optional
+        maximum allowable width of the scene bounds, by default 20
 
-    Args:
-        bounds (list or tuple): Bounds in 4326
-        max_scene_width (int, optional): maxumum width of bounds in 
-        lon degrees. Defaults to 20.
-
-    Returns:
-        bool: True if crosses the antimeridian
+    Returns
+    -------
+    bool
+        if the bounds cross the antimeridian
     """
 
     min_x = -180 + max_scene_width # -160
@@ -181,15 +260,18 @@ def split_s1_bounds_at_am_crossing(bounds: tuple, lat_buff : float = 0) -> list[
     """split the s1 bounds into bounds on the left and
     right of the antimeridian.
 
-    Args:
-        scene_polygon (shapely.polygon): polygon of the scene from asf
-        lat_buff (float): A lattitude degrees buffer to add/subtract from
-                            max and min lattitudes 
+    Parameters
+    ----------
+    bounds : tuple
+        the set of bounds (min_lon, min_lat, max_lon, max_lat)
+    lat_buff : float, optional
+        An additional buffer to subract from lat, by default 0.
 
-    Returns:
-    list(tuple) : a list containing two sets of bounds for the left
-                    and right of the antimeridian
-    """
+    Returns
+    -------
+    list[tuple]
+        a list containing two sets of bounds for the left and right of the antimeridian
+    """ 
     max_negative_x = min([x for x in [bounds[0],bounds[2]] if x < 0])
     min_positive_x = min([x for x in [bounds[0],bounds[2]] if x > 0])
     min_y = min([bounds[1],bounds[3]]) - lat_buff
@@ -200,19 +282,48 @@ def split_s1_bounds_at_am_crossing(bounds: tuple, lat_buff : float = 0) -> list[
     bounds_right = (min_positive_x, min_y, 180, max_y)
     return [tuple(bounds_left), tuple(bounds_right)]
 
-def get_target_antimeridian_projection(bounds):
+def get_target_antimeridian_projection(bounds: tuple) -> int:
     """depending where were are on the earth, the desired
     crs at the antimeridian will change. e.g. polar stereo
     is desired at high and low lats, local utm zone elsewhere
-    (e.g. at the equator)"""
+    (e.g. at the equator).
+
+    Parameters
+    ----------
+    bounds : tuple
+        the set of bounds (min_lon, min_lat, max_lon, max_lat)
+
+    Returns
+    -------
+    int
+        The CRS in integer form: e.g. 3031
+    """
     min_lat = min(bounds[1], bounds[3])
     target_crs =  3031 if min_lat < -50 else 3995 if min_lat > 50 else get_local_utm(bounds, antimeridian=True)
     return target_crs
 
-def make_empty_cop30m_profile(bounds):
+def make_empty_cop30m_profile(bounds: tuple) -> dict:
+    """make an empty cop30m dem rasterio profile based on a set of bounds.
+    The desired pixel spacing changes based on lattitude
+    see : https://copernicus-dem-30m.s3.amazonaws.com/readme.html
 
-    # the pixel spacing changes based on lattitude
-    # https://copernicus-dem-30m.s3.amazonaws.com/readme.html
+    Parameters
+    ----------
+    bounds : tuple
+        the set of bounds (min_lon, min_lat, max_lon, max_lat)
+
+    Returns
+    -------
+    dict
+        A rasterio profile
+
+    Raises
+    ------
+    ValueError
+        If the latitude of the supplied bounds cannot be
+        associated with a target pixel size
+    """
+
     lon_res = -0.0002777777777777778
     mean_lat = abs((bounds[1] + bounds[3])/2)
     if mean_lat < 50:
@@ -237,8 +348,8 @@ def make_empty_cop30m_profile(bounds):
         'driver': 'GTiff', 
         'dtype': 'float32', 
         'nodata': np.nan, 
-        'height': int((bounds[3] - bounds[1]) / lat_res), 
-        'width': int((bounds[2] - bounds[0]) / lon_res), 
+        'height': abs(int((bounds[3] - bounds[1]) / lat_res)), 
+        'width': abs(int((bounds[2] - bounds[0]) / lon_res)), 
         'count': 1, 
         'crs': CRS.from_epsg(4326), 
         'transform': transform, 
