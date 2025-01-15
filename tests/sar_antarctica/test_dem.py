4d9259018e0c4a59213dfc07e74e4f908fa27be5
abradley60 <alexbradley60@gmail.com>
2025-01-16 09:23:33 +1100

    cleanup tests

--- tests/sar_antarctica/test_dem.py
+++ tests/sar_antarctica/test_dem.py
@@ -130,17 +130,18 @@ def test_remove_geoid(bounds, trg_shape, geoid_ref_mean, ellipsoid_ref_mean):
     assert np.mean(dem_arr_ellipsoid) == ellipsoid_ref_mean
 
 
-@pytest.mark.parametrize("bounds, trg_shape, trg_crs", [
-    ((-179.9, -79.2, -179.1, -79.1), (1, 366, 966), 4326),
-    ((178.1, -79.2, 179.95, -79.1), (1, 366, 2226), 4326), 
-    ((-179.2, -79.2, 179.1, -79.1), (1, 1275, 1501), 3031) # across antimeridian
+@pytest.mark.parametrize("bounds, ellipsoid_heights, trg_shape, trg_crs", [
+    ((-179.9, -79.2, -179.1, -79.1), False, (1, 366, 966), 4326),
+    ((178.1, -79.2, 179.95, -79.1), False, (1, 366, 2226), 4326), 
+    ((-179.2, -79.2, 179.1, -79.1), False, (1, 1266, 1502), 3031), # across antimeridian
+    ((-179.2, -79.2, 179.1, -79.1), True, (1, 1266, 1502), 3031) # across antimeridian
     ])
-def test_get_cop30_dem_for_bounds(bounds, trg_shape, trg_crs):
+def test_get_cop30_dem_for_bounds(bounds, ellipsoid_heights, trg_shape, trg_crs):
     os.makedirs(CURRENT_DIR / Path('TMP'), exist_ok=True)
     dem_arr, dem_profile = get_cop30_dem_for_bounds(
         bounds, 
         save_path= CURRENT_DIR / Path('TMP') / Path('TMP.tif'), 
-        ellipsoid_heights=False, 
+        ellipsoid_heights=ellipsoid_heights, 
         buffer_pixels=2,
         COP30_VRT_PATH=TEST_COP30_VRT_PATH,
         GEOID_TIF_PATH=TEST_GEOID_PATH,
@@ -148,37 +149,4 @@ def test_get_cop30_dem_for_bounds(bounds, trg_shape, trg_crs):
     )
     assert dem_arr.shape == trg_shape
     assert dem_profile['crs'].to_epsg() == trg_crs
-
-    
-if __name__ == "__main__":
-
-    from sar_antarctica.nci.preparation.dem import (
-        get_cop30_dem_for_bounds,
-        find_required_dem_tile_paths_by_filename,
-        expand_bounds
-    )
-
-    import logging
-    logging.basicConfig()
-    logging.getLogger().setLevel(logging.INFO)
-    from shapely.geometry import box
-    import geopandas as gpd
-
-    # bounds = (163.121597, -78.632782, 172.382263, -76.383263) # full s1 scene
-    # bounds = (165, -76.632782, 170, -75) # smaller area
-    # bounds = (-177.884048, -78.176201, 178.838364, -75.697151) # full AM scene bounds
-    # bounds = (-179.2, -79.2, 179.1, -79.1) # smaller AM bounds
-    # bounds = (140, -66, 141, -65) # smaller area over water
-    # bounds = (20.1, -75.2, 22.2, -73.1) 
-    # bounds = (-22.2, -75.2, -20.1, -73.1) 
-    bounds = ((-179.2, -79.2, 179.1, -79.1))
-
-    # dem_paths = find_required_dem_tile_paths_by_filename(bounds)
-    # print(f'{len(dem_paths)} tiles found')
-    # print(dem_paths)
-    dem_arr, dem_profile = get_cop30_dem_for_bounds(bounds, save_path='dem_tmp.tif', ellipsoid_heights=False, buffer_pixels=2)
-
-    # save bounds for exploration
-    # gdf = gpd.GeoDataFrame([{"geometry": box(*bounds)}], crs="EPSG:4326")
-    # # Write the GeoDataFrame to a GeoJSON file
-    # gdf.to_file("bounding_box.geojson", driver="GeoJSON")
+    shutil.rmtree(CURRENT_DIR / Path('TMP'))
