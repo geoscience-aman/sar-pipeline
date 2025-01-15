5cc97fd9e3eced9e367326b5ab4beb71aa93def9
Caitlin Adams <25995927+caitlinadams@users.noreply.github.com>
2025-01-16 10:54:12 +1100

    Split functions to support tests

--- sar_antarctica/nci/preparation/create_dem_vrt.py
+++ sar_antarctica/nci/preparation/create_dem_vrt.py
@@ -1,8 +1,9 @@
 import os
 from pathlib import Path
+from typing import Generator
 
-def build_vrt(source_dir: Path, pattern: str, vrt_path: str | os.PathLike):
-    """Generic function for building a VRT from files matching a given pattern in a source directory
+def find_tiles_for_vrt(source_dir: Path, pattern: str) -> Generator[Path, None, None]:
+    """_summary_
 
     Parameters
     ----------
@@ -10,13 +11,33 @@ def build_vrt(source_dir: Path, pattern: str, vrt_path: str | os.PathLike):
         The directory to search for files
     pattern : str
         The pattern to search for
-        e.g. Copernicus_DSM_COG_10_S??_00_????_00_DEM/*.tif
-    vrt_path : str | os.PathLike
-        Where to write the VRT to
+        e.g. DEM_folder/*.tif
+
+    Returns
+    -------
+    Generator[Path, None, None]
+        A generator that yeilds `Path` objects for all files matching the pattern
+        e.g. /path/to/DEM_folder/DEM_file.tif
     """
 
     tiles = source_dir.rglob(pattern)
 
+    return tiles
+
+def build_vrt(tiles: Generator[Path, None, None], vrt_path: str | os.PathLike, run: bool = True):
+    """Generic function for building a VRT from files matching a given pattern in a source directory
+
+    Parameters
+    ----------
+    tiles : Generator[Path, None, None]
+        A generator that yeilds `Path` objects for tiles
+        e.g. /path/to/DEM_folder/DEM_file.tif
+    vrt_path : str | os.PathLike
+        Where to write the VRT to
+    run : bool, optional
+        Whether to run the step to create the VRT, by default True
+        Can use False to generate the temporary file and check
+    """
     with open("vrt_temp.txt", "w") as f:
         f.writelines(f"{tile}\n" for tile in tiles)
 
@@ -32,4 +53,6 @@ def create_glo30_dem_south_vrt():
     PATTERN = "Copernicus_DSM_COG_10_S??_00_????_00_DEM/*.tif"
     VRT_PATH = Path("/g/data/yp75/projects/ancillary/dem/copdem_south.vrt")
 
-    build_vrt(SOURCE_DIR, PATTERN, VRT_PATH)
+    tiles = find_tiles_for_vrt(SOURCE_DIR, PATTERN)
+
+    build_vrt(tiles, VRT_PATH)
