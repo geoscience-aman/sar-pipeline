bf5629cd42c9628a2b3bb809de9a1ebb23db2746
Caitlin Adams <25995927+caitlinadams@users.noreply.github.com>
2025-01-16 13:19:20 +1100

    Add tests for creating vrt and tile index

--- sar_antarctica/nci/preparation/create_dem_vrt.py
+++ sar_antarctica/nci/preparation/create_dem_vrt.py
@@ -2,7 +2,7 @@ import os
 from pathlib import Path
 from typing import Generator
 
-def find_tiles_for_vrt(source_dir: Path, pattern: str) -> Generator[Path, None, None]:
+def find_tiles(source_dir: Path, pattern: str) -> Generator[Path, None, None]:
     """_summary_
 
     Parameters
@@ -24,13 +24,13 @@ def find_tiles_for_vrt(source_dir: Path, pattern: str) -> Generator[Path, None, 
 
     return tiles
 
-def build_vrt(tiles: Generator[Path, None, None], vrt_path: str | os.PathLike, run: bool = True):
+def build_vrt(tiles: Generator[Path, None, None] | list[Path], vrt_path: str | os.PathLike, run: bool = True):
     """Generic function for building a VRT from a generator of tile paths
 
     Parameters
     ----------
-    tiles : Generator[Path, None, None]
-        A generator that yeilds `Path` objects for tiles
+    tiles : Generator[Path, None, None] | list[Path]
+        A generator (or list) that provides `Path` objects for tiles
         e.g. /path/to/DEM_folder/DEM_file.tif
     vrt_path : str | os.PathLike
         Where to write the VRT to, ending in .vrt
@@ -38,13 +38,13 @@ def build_vrt(tiles: Generator[Path, None, None], vrt_path: str | os.PathLike, r
         Whether to run the step to create the VRT, by default True
         Can use False to generate the temporary file and check
     """
-    with open("vrt_temp.txt", "w") as f:
+    with open("temp.txt", "w") as f:
         f.writelines(f"{tile}\n" for tile in tiles)
 
     if run:
-        os.system(f'gdalbuildvrt -input_file_list vrt_temp.txt {vrt_path}')
+        os.system(f'gdalbuildvrt -input_file_list temp.txt {vrt_path}')
 
-        os.remove("vrt_temp.txt")
+        os.remove("temp.txt")
 
 def build_tileindex(tiles: Generator[Path, None, None], tindex_path: str | os.PathLike, run: bool = True):
     """Generic function for building a tile index from a generator of tile paths
@@ -60,13 +60,13 @@ def build_tileindex(tiles: Generator[Path, None, None], tindex_path: str | os.Pa
         Whether to run the step to create the tile index, by default True
         Can use False to generate the temporary file and check
     """
-    with open("tindex_temp.txt", "w") as f:
+    with open("temp.txt", "w") as f:
         f.writelines(f"{tile}\n" for tile in tiles)
 
     if run:
-        os.system(f'gdaltindex {tindex_path} --optfile tindex_temp.txt')
+        os.system(f'gdaltindex {tindex_path} --optfile temp.txt')
 
-        os.remove("tindex_temp.txt")
+        os.remove("temp.txt")
 
 def create_glo30_dem_south_vrt():
     """Create a VRT for the Copernicus Global 30m DEM on NCI
@@ -76,6 +76,6 @@ def create_glo30_dem_south_vrt():
     PATTERN = "Copernicus_DSM_COG_10_S??_00_????_00_DEM/*.tif"
     VRT_PATH = Path("/g/data/yp75/projects/ancillary/dem/copdem_south.vrt")
 
-    tiles = find_tiles_for_vrt(SOURCE_DIR, PATTERN)
+    tiles = find_tiles(SOURCE_DIR, PATTERN)
 
     build_vrt(tiles, VRT_PATH)
