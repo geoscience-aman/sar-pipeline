7cfce5f3f63cfe19538910530fc3bc81d865888d
Caitlin Adams <25995927+caitlinadams@users.noreply.github.com>
2025-01-16 16:38:20 +1100

    Update submission scripts to use a job config file

--- sar_antarctica/nci/submission/pyrosar_gamma/pyrosar_gamma.py
+++ sar_antarctica/nci/submission/pyrosar_gamma/pyrosar_gamma.py
@@ -1,6 +1,8 @@
 import click
 import os
 from pathlib import Path
+import tomli
+from typing import Any
 
 WORKFLOW = "pyrosar_gamma"
 PROCESSING_DIR = "/g/data/yp75/projects/sar-antractica-processing"
@@ -36,45 +38,72 @@ def get_list_of_scenes(scene_source: str) -> list[str]:
     else:
         raise RuntimeError("No valid scenes were found for processing. Expected single .zip file or .txt file containing at least one .zip file.")
 
-def update_pbs_template(pbs_template: str, processing_dir: str, scene_id: str, workflow_id: str, ncpu: int=4, mem:int=32) -> str:
-    """For a given PBS jobscript template, replace specified values
+def update_pbs_template(pbs_template: str, scene_id: str, job_config: dict[str, str | dict[str, Any]]) -> str:
+    """_summary_
 
     Parameters
     ----------
     pbs_template : str
         A string containing a PBS jobscript
-    processing_dir : str
-        The directory to store configuration and jobscript files
     scene_id : str
-        The scene id, used to name the scene configuration file
-    workflow_id : str
-        The name of the workflow, used to name the workflow configuration fie
-    ncpu : int, optional
-        The number of CPU to use in the jobscript, by default 4
-    mem : int, optional
-        The amount of memory (in GB) to use in the jobscript, by default 32
+        The scene ID for the job
+    job_config : dict[str, str  |  dict[str, Any]]
+        Dictionary containing information on the job, main keys are
+        root, submission, configuration, and settings
 
     Returns
     -------
     str
         The updated PBS jobscript string with specified values replaced
     """
-    # Replace PBS Resource variables
-    
-    pbs_template = pbs_template.replace("<NCPU>", str(ncpu))
-    pbs_template = pbs_template.replace("<MEM>", str(mem))
-    pbs_template = pbs_template.replace("<PROCESSING_DIR>", processing_dir)
-    pbs_template = pbs_template.replace("<SCENE_ID>", scene_id)
-    pbs_template = pbs_template.replace("<WORKFLOW_CONFIG>", f"{processing_dir}/config/workflow_config/{workflow_id}.toml")
-    pbs_template = pbs_template.replace("<SCENE_CONFIG>", f"{processing_dir}/config/scene_config/{scene_id}.toml")
+
+    """For a given PBS jobscript template, replace specified values with jobscript settings
+
+    Parameters
+    ----------
+    pbs_template : str
+        A string containing a PBS jobscript
+    jobscript_settings: dict
+
+
+    Returns
+    -------
+    str
+        The updated PBS jobscript string with specified values replaced
+    """
+
+    processing_path = Path(job_config["root"])
+    log_path = processing_path / job_config["submission"]["root"] / job_config["submission"]["logs"]
+    config_path = processing_path / job_config["configuration"]["root"]
+
+    job_configuration = job_config["configuration"]
+    job_settings = job_config["settings"]
+
+    workflow_config = job_settings["workflow_config"]
+    # Dictionary to replace placeholders in PBS text with values from configurations
+    replace_dict = {
+        "<SCENE_ID>": scene_id,
+        "<NCPU>": job_settings["ncpu"],
+        "<MEM>": job_settings["mem"],
+        "<QUEUE>": job_settings["queue"],
+        "<PROJECT>": job_settings["project"],
+        "<WALLTIME>": job_settings["walltime"],
+        "<STORAGE>": job_settings["storage"],
+        "<LOG_DIR>": log_path,
+        "<WORKFLOW_CONFIG>": config_path / job_configuration["workflow"] / f"{workflow_config}.toml",
+        "<SCENE_CONFIG>": config_path / job_configuration["scene"] / f"{scene_id}.toml"
+    }
+
+    for key, value in replace_dict.items():
+        pbs_template = pbs_template.replace(key, value if isinstance(value, str) else str(value))
 
     return pbs_template
 
 
 @click.command()
-@click.argument("processing_dir", nargs=1)
+@click.argument("config_file", nargs=1)
 @click.argument("scene_source", nargs=1)
-def pyrosar_gamma_workflow(processing_dir:str , scene_source: str) -> None:
+def pyrosar_gamma_workflow(config_file: str | os.PathLike, scene_source: str | os.PathLike) -> None:
     """Take an input of a single scene or file with multiple scenes and submit pyroSAR+GAMMA jobs
 
     Parameters
@@ -85,10 +114,22 @@ def pyrosar_gamma_workflow(processing_dir:str , scene_source: str) -> None:
         The file to be processed. Either a single .zip or a .txt containing multiple .zip files
     """
 
-    # Get folder structure
-    submission_dir = Path(processing_dir) / "submission"
-    log_dir = submission_dir / "logs"
+    current_file_directory = Path(__file__).resolve().parent
 
+    with open(config_file, "rb") as f:
+        config = tomli.load(f)   
+
+    # Extract specific configuration dictionaries
+    job_config = config["job"]
+    submission_config = job_config["submission"]
+    configuration_config = job_config["configuration"]
+    settings_config = job_config["settings"]
+
+    # Get folder structure
+    processing_dir = Path(job_config["root"])
+    log_dir = processing_dir / submission_config["root"] / submission_config["logs"]
+
+    # Get scenes from source
     scene_list = get_list_of_scenes(scene_source)
 
     for scene_path in scene_list:
@@ -98,23 +139,22 @@ def pyrosar_gamma_workflow(processing_dir:str , scene_source: str) -> None:
         scene_script.parent.mkdir(exist_ok=True, parents=True)
 
         # Read the workflow template and replace values
-        template_file = Path(f"{WORKFLOW}.txt")
+        workflow_name = settings_config['workflow_config']
+        template_file = current_file_directory / f"{workflow_name}.txt"
+        print(template_file)
         pbs_template = template_file.read_text()
         pbs_template = update_pbs_template(
             pbs_template, 
-            processing_dir, 
             scene_id, 
-            WORKFLOW, 
-            ncpu=4, 
-            mem=64)
+            job_config
+        )
 
         # Write updated text to pbs script
         scene_script.write_text(pbs_template)
 
         # Submit script
         qsub_command = f"qsub {scene_script}"
-        print(qsub_command)
-        #os.system(qsub_command)
+        os.system(qsub_command)
 
 
 if __name__ == "__main__":
